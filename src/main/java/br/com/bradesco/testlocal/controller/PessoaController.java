9ea17bde0f20a7d324b4c3a762f7309882a62eed
Wilkinson Saturnino <wilkinson.wkn@gmail.com>
2021-05-05 15:59:55 -0300

    Iniciando documentação com Swagger-Jax-Rs. Ainda não está funcionando.

--- src/main/java/br/com/bradesco/testlocal/controller/PessoaController.java
+++ src/main/java/br/com/bradesco/testlocal/controller/PessoaController.java
@@ -3,40 +3,62 @@ package br.com.bradesco.testlocal.controller;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.com.bradesco.testlocal.domain.Pessoa;
 import br.com.bradesco.testlocal.repository.PessoaRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Path(value = "/api/v1")
+@OpenAPIDefinition(info = @Info(title = "[Pagamentos Pix API]: Microserviço que faz a persistência dos dados da entidade Pessoa.\"", version = "1.0.0", description = "Este swagger é gerado automaticamente com uso da biblioteca swagger-jaxrs2.", license = @License(name = "OpenSource"), contact = @Contact(name = "Wilkinson Saturnino Dev")), servers = { @Server(url = "") })
 public class PessoaController {
 
 	@Inject
 	private PessoaRepository pessoaRepository;
 
-	/* Listar todos os Usuários */
 	@GET
 	@Path(value = "/usuarios")
 	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(summary = "Lista todos os Usuários.", description = "Lista todos os Usuários.", responses = {
+			@ApiResponse(responseCode = "200", description = "Retorno de sucesso.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "401", description = "Você não tem autorização.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "403", description = "Proibido de ver estar informações.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "404", description = "O recurso não foi encontrado.", content = @Content(mediaType = "application/json")), 
+			@ApiResponse(responseCode = "422", description = "O item não foi encontrado.", content = @Content(mediaType = "application/json")), })
 	public List<Pessoa> listarTodos() {
 		return pessoaRepository.findAll();
 	}
 
-	/* Salvar Usuário */
 	@POST
 	@Path(value = "/usuarios")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(summary = "Salva Usuário.", description = "Salva Usuário.", responses = {
+			@ApiResponse(responseCode = "200", description = "Retorno de sucesso.", 
+					content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = Pessoa.class))),
+			@ApiResponse(responseCode = "201", description = "Recurso criado com sucesso.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "401", description = "Você não tem autorização.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "403", description = "Proibido de ver estar informações.", content = @Content(mediaType = "application/json")),
+			@ApiResponse(responseCode = "404", description = "O recurso não foi encontrado.", content = @Content(mediaType = "application/json")), 
+			@ApiResponse(responseCode = "422", description = "O item não foi encontrado.", content = @Content(mediaType = "application/json")), })	
 	public Pessoa salvarPessoa(Pessoa pessoa) {
 		return pessoaRepository.save(pessoa);
 	}
