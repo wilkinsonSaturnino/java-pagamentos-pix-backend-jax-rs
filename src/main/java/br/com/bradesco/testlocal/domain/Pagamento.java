7f4106b51cf1bb72f3df104fa2c24b122e27bc1a
Wilkinson Saturnino <wilkinson.wkn@gmail.com>
2023-06-27 06:21:14 -0300

    Removendo importações não utilizadas. Atualizando senha do banco de dados local.

--- src/main/java/br/com/bradesco/testlocal/domain/Pagamento.java
+++ src/main/java/br/com/bradesco/testlocal/domain/Pagamento.java
@@ -16,11 +16,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 
 @Entity
 @Table(name = "pagamento")
--- src/main/java/br/com/bradesco/testlocal/domain/Pessoa.java
+++ src/main/java/br/com/bradesco/testlocal/domain/Pessoa.java
@@ -13,10 +13,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "pessoa")
